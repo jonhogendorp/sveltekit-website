4f0b2e3727f51ebfd8b3c9a74295e217468d1cfb
Jon Hogendorp <jonhoog@live.nl>
2023-10-16 16:49:15 +0200

    updating styles and expanding components

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,10 +1,47 @@
+<script>
+
+</script>
 <div class="hero-image">
     <h1 class="landing-text">
       Welcome to my Portfolio site
     </h1>
   </div>
   <div class="content">
-        <div class="landing-content"></div>
+        <div class="landing-content">
+            <div class="intro intro-left">
+                <h2>Hi my name is Jon</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam quisquam consequuntur distinctio similique minus dolore sapiente veniam amet repellendus odio eos quis, temporibus eius harum nobis quam quos tenetur saepe!</p>
+            </div>
+            <div class="intro intro-right"></div>
+        </div>
         <div class="landing-content bg-slate-50"></div>
         <div class="landing-content"></div>
   </div>
+
+<style>
+    :global(.landing-content){
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+    :global(.intro){
+        width: 35%;
+        height: 60%;
+        min-height: 400px;
+        min-width: 400px;
+        padding:1.5rem;
+    }
+    :global(.intro-left){
+        background-color: rgb(212, 216, 230);
+        border-top-left-radius: 1rem;
+        border-bottom-left-radius: 1rem;
+    }
+    :global(.intro-right){
+        background-color: rgb(78,84,100); 
+        border-top-right-radius: 1rem;
+        border-bottom-right-radius: 1rem;
+    }
+    
+
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,33 @@
 <script>
 	import Navbar from "../components/Navbar.svelte";
     import "../app.css";
-  import Footer from "../components/Footer.svelte";
+    import Footer from "../components/Footer.svelte";
+
+    let y;
+    let innerHeight = 0;
+    let innerWidth = 0;
+
+    function goTop() {
+        document.body.scrollIntoView();
+    }
 </script>
   
 <svelte:head>
     <title>Brand website Jon</title>
 </svelte:head> 
 
+<div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + (
+    y > 0 ? 'opacity-full pointer-events-auto' : 'pointer-events-none opacity-0'
+)}>
+    <button on:click={goTop}>
+        <i class="fa fa-arrow-up fa-lg"></i>
+    </button>
+</div>
+
 <Navbar/> 
 
 <slot /> 
 
 <Footer/>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
